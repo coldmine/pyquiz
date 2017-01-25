b9fe4c072b39b251802cfed91aca429ac9c54c76
kim yongbin <kybinz@gmail.com>
2017-01-26 01:01:05 +0900

    lv1.py 간소화

--- answer.py
+++ answer.py
@@ -1,13 +1,12 @@
 # coding=utf8
 
-import lv1
+import lv1 as q
 
 def ans(a, b):
 	pass
 	# 여기에 답변 코드를 작성하세요.
 
 def main():
-	q = lv1.quiz()
 	print(q.question)
 	err = q.answer(ans)
 	if err:
--- lv1.py
+++ lv1.py
@@ -2,18 +2,17 @@
 
 from collections import namedtuple
 
-class quiz:
-	def __init__(self):
-		self.question = "두 수를 더한 수를 반환하는 함수를 작성하세요."
-		test = namedtuple("test", "a b want")
-		self.tests = [
-			test(a=1, b=2, want=3),
-			test(a=-1, b=1, want=0),
-			test(a=-2, b=-2, want=-4),
-		]
-	def answer(self, func):
-		for t in self.tests:
-			got = func(t.a, t.b)
-			if got != t.want:
-				return "답변한 함수에 ({0}, {1})를 넣었더니 {2}가 나왔습니다.\n원하는 값 {3}".format(t.a, t.b, got, t.want)
+question = "두 수를 더한 수를 반환하는 함수를 작성하세요."
+test = namedtuple("test", "a b want")
+tests = [
+	test(a=1, b=2, want=3),
+	test(a=-1, b=1, want=0),
+	test(a=-2, b=-2, want=-4),
+]
+
+def answer(func):
+	for t in tests:
+		got = func(t.a, t.b)
+		if got != t.want:
+			return "답변한 함수에 ({0}, {1})를 넣었더니 {2}가 나왔습니다.\n원하는 값 {3}".format(t.a, t.b, got, t.want)
 
